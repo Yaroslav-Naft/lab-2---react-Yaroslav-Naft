be880d9cff1e0a915d96fbc3920322e0ad60cdfc
Yaroslav-Naft <ynaftulyev@my.bcit.ca>
2020-11-16 21:00:36 -0800

    added test data

---
+++ my-app/src/data/movieData.js
@@ -0,0 +1,19 @@
+const movieData = [
+{
+    moviePoster: "poster0",
+    movieTitle: "title0",
+    movieReleaseDate: "January 17 20200"
+},
+{
+    moviePoster: "poster1",
+    movieTitle: "title1",
+    movieReleaseDate: "January 17 20201"
+},
+{
+    moviePoster: "poster2",
+    movieTitle: "title2",
+    movieReleaseDate: "January 17 20202"
+}
+]
+
+export default movieData;
