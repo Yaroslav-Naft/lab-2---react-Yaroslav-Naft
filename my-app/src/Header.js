fa4ce29e5bfdd9dac0f73a6ffb6baaa58bdbc37c
Yaroslav-Naft <ynaftulyev@my.bcit.ca>
2020-11-16 16:43:38 -0800

    Added Header and tested it

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -4,7 +4,7 @@ export default class Header extends Component {
     render() {
         return (
             <div>
-              <h1>Hello World</h1>
+              <h1>Hello World Yaro!</h1>
             </div>
         )
     }
