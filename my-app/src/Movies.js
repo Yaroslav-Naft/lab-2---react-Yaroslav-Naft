e5afa858ffb29bec1c9ff2abf9f50785dab0df35
Yaroslav-Naft <ynaftulyev@my.bcit.ca>
2020-11-16 21:45:39 -0800

    manually added movies withtouth mapping

--- my-app/src/Movies.js
+++ my-app/src/Movies.js
@@ -13,33 +13,25 @@ function initiateMovie(){
 export default class Movies extends Component {
     render() {
         return (
-            <div>
-                <h1>Hello</h1>
                 <div class="container">
-                <div>Poster:{movieData[0].moviePoster}</div>
-                <div>Title:{movieData[0].movieTitle}</div>
-                <div>Date:{movieData[0].movieReleaseDate}</div>
-                </div>
+                    <div class="row my-2 mx-2">
                 <MoviesItem 
                 poster = {movieData[0].moviePoster}
                 title = {movieData[0].moviePoster}
                 date = {movieData[0].moviePoster}
                 />
-
-                <div class="container">
-                <div>Poster:{movieData[1].moviePoster}</div>
-                <div>Title:{movieData[1].movieTitle}</div>
-                <div>Date:{movieData[1].movieReleaseDate}</div>
+                <MoviesItem 
+                poster = {movieData[1].moviePoster}
+                title = {movieData[1].moviePoster}
+                date = {movieData[1].moviePoster}
+                />
+                <MoviesItem 
+                poster = {movieData[2].moviePoster}
+                title = {movieData[2].moviePoster}
+                date = {movieData[2].moviePoster}
+                />
+                   </div>
                 </div>
-
-                <div class="container">
-                <div>Poster:{movieData[2].moviePoster}</div>
-                <div>Title:{movieData[2].movieTitle}</div>
-                <div>Date:{movieData[2].movieReleaseDate}</div>
-                </div>
-
-
-            </div>
         )
     }
 }
--- my-app/src/MoviesItem.js
+++ my-app/src/MoviesItem.js
@@ -9,9 +9,7 @@ export default class MoviesItem extends Component {
                 <a href="index.html" class="img-wrap"> 
                 <img src={this.props.poster} class="card-img-top image" alt="product"/> 
                 </a>
-
                 <button type="button" class="btn btn-dark py-2 btn-block">See Movie</button>
-
                 <div class="card-body border-top text-center">
                 <div class="card-text">
                     <a href="index.html" class="card-link title"><h5 class="card-title text-muted">{ this.props.title }</h5></a>
