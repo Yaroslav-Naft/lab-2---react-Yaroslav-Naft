68c42ba30a376b66475da2b799255954a5efe5c2
Yaroslav-Naft <ynaftulyev@my.bcit.ca>
2020-11-16 16:50:38 -0800

    Added Movies

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { React } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import Movies from "./Movies";
 
 function App() {
   return (
    <div className="App">
     <Header />
+    <Movies />
     <Footer />
   </div>
   );
