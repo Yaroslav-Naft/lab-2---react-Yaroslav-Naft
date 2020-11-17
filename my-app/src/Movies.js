5e5da2cd27918cf44fefa53b77e4a31adc1f69f3
Yaroslav-Naft <ynaftulyev@my.bcit.ca>
2020-11-16 21:07:43 -0800

    testing data passing

--- my-app/src/Movies.js
+++ my-app/src/Movies.js
@@ -5,6 +5,25 @@ export default class Movies extends Component {
         return (
             <div>
                 <h1>Hello</h1>
+                <div class="container">
+                <span>Poster:{movieData.moviePoster[0]}</span>
+                <span>Title:{movieData.movieTitle[0]}</span>
+                <span>Date:{movieData.movieReleaseDate[0]}</span>
+                </div>
+
+                <div class="container">
+                <span>Poster:{movieData.moviePoster[1]}</span>
+                <span>Title:{movieData.movieTitle[1]}</span>
+                <span>Date:{movieData.movieReleaseDate[1]}</span>
+                </div>
+
+                <div class="container">
+                <span>Poster:{movieData.moviePoster[2]}</span>
+                <span>Title:{movieData.movieTitle[2]}</span>
+                <span>Date:{movieData.movieReleaseDate[2]}</span>
+                </div>
+
+
             </div>
         )
     }
