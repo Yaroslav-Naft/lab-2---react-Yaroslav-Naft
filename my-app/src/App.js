a5d53112881ab1724820483b02ceef5edd7bfad3
Yaroslav-Naft <ynaftulyev@my.bcit.ca>
2020-11-16 16:55:17 -0800

    Added DropDown List to app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,13 @@ import { React } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Movies from "./Movies";
+import DropdownMenu from "./DropdownMenu";
 
 function App() {
   return (
    <div className="App">
     <Header />
+
     <Movies />
     <Footer />
   </div>
---
+++ my-app/src/DropdownMenu.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class DropdownMenu extends Component {
+    render() {
+        return (
+            <div>
+             <h1>DropDown Menu</h1>   
+            </div>
+        )
+    }
+}
