adcf56c017479614f88874d25ca28645f283be85
Yaroslav-Naft <ynaftulyev@my.bcit.ca>
2020-11-16 17:20:23 -0800

    commit

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,16 @@
-import 'bootstrap/dist/js/bootstrap';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import 'jquery';
 import 'popper.js';
-
+import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals'; 
 
 ReactDOM.render(
-  <App />, document.getElementById('root')
+  <App />,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
