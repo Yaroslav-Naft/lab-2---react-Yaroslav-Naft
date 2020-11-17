22d0003950a3f3f4fed3cbfa5f389a2f38c87f66
Yaroslav-Naft <ynaftulyev@my.bcit.ca>
2020-11-16 16:46:13 -0800

    Added Footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { React } from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return (
    <div className="App">
     <Header />
+    <Footer />
   </div>
   );
 }
