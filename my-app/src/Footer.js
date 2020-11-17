1dce8fc6b22d13cc2a511ad8003765cdd467ec11
Yaroslav-Naft <ynaftulyev@my.bcit.ca>
2020-11-16 16:45:10 -0800

    Added Footer

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
-export default class Header extends Component {
+export default class Footer extends Component {
     render() {
         return (
             <div>
-              <h1>Header</h1>
+            <h1>Footer</h1>    
             </div>
         )
     }
