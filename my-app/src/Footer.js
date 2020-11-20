a46a8b5e002b98858718e4e95e3d5ec248d1458d
Yaroslav-Naft <ynaftulyev@my.bcit.ca>
2020-11-20 13:01:29 -0800

    commit

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -4,7 +4,99 @@ export default class Footer extends Component {
     render() {
         return (
             <div>
-            <h1 class="display-4">Footers</h1>    
+
+
+{/* <!-- Footer --> */}
+    <footer>
+      <div class="container">
+        <div class="about-footer">
+          <h5>About Us</h5>
+          <ul>
+            <li><a href="#">Who We Are</a></li>
+            <li><a href="#">Our Store</a></li>
+            <li><a href="#">Stores Near to Me</a></li>
+          </ul>
+        </div>
+        {/* <!--End of the First Footer Column--> */}
+
+        <div class="cart-footer">
+          <h5>Customer Care</h5>
+          <ul>
+            <li><a href="#">Gift Cards</a></li>
+            <li><a href="#">Ordering</a></li>
+            <li><a href="#">Payment</a></li>
+            <li><a href="#">Shipping</a></li>
+            <li><a href="#">Returns</a></li>
+          </ul>
+        </div>
+        {/* <!--End of the Second Footer Column--> */}
+
+        <div class="social-footer">
+          <h5>Follow us</h5>
+          <ul>
+          <li><a href="#"><i class="fa fa-facebook-square" style="font-size:30px"></i></a></li>
+                  <li><a href="#"><i class="fa fa-linkedin-square" style="font-size:30px"></i></a></li>
+                 <li> <a href="#"><i class="fa fa-twitter-square" style="font-size:30px"></i></a></li>
+              </ul>
+
+      </div>
+        {/* <!--End of the Third Footer Column--> */}
+
+        <div class="contact-us-footer">
+          <ul class="contact">
+            <h5>Contact Us</h5>
+            <li
+              ><a href="#"
+                ><i class="fa fa-envelope" style="font-size: 30px"></i
+              ></a>
+            </li>
+            <li
+              ><a href="#"
+                ><i class="fa fa-phone" style="font-size: 30px"></i></a
+            ></li>
+            <li
+              ><a href="#"
+                ><i class="fa fa-map-marker" style="font-size: 30px"></i></a
+            ></li>
+          </ul>
+        </div>
+
+        <div class="map">
+          <h6>1111, Seymor Street, Vancouver, BC</h6>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10410.510865752853!2d-123.1152548!3d49.2834511!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb4e03bf432e66714!2sBCIT%20Downtown%20Campus!5e0!3m2!1sen!2sca!4v1602882765225!5m2!1sen!2sca"
+            width="230"
+            height="100"
+            frameborder="0"
+            style="border: 0"
+            allowfullscreen=""
+            aria-hidden="false"
+            tabindex="0"
+          ></iframe>
+        </div>
+      </div>
+      {/* <!-- End of Container Footer --> */}
+
+      {/* <!-- Copyright--> */}
+      <div class="copyright">
+        <p class="m-0 text-center text-white"
+          >Copyright &copy; <a href="index.html">WalkaBooks 2020</a> <br />
+          <a href="#">Terms & Conditions</a> | <a href="#">Privacy Policy </a> |
+          <a href="#"> Site Map </a> |
+          <a href="credits.html">Photo Credits</a></p
+        >
+      </div>
+      {/* <!-- End of Copyright --> */}
+    </footer>
+    {/* <!-- End of Footer --> */}
+
+     
+
+
+
+
+
+
             </div>
         )
     }
