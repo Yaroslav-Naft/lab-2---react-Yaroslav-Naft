bb0b22b8e79054208fc074585d7fce14032627f5
Yaroslav-Naft <ynaftulyev@my.bcit.ca>
2020-11-20 13:06:17 -0800

    commit

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -42,7 +42,7 @@ export default class Footer extends Component {
       </div>
         {/* <!--End of the Third Footer Column--> */}
 
-        <div class="contact-us-footer">
+        {/* <div class="contact-us-footer">
           <ul class="contact">
             <h5>Contact Us</h5>
             <li
@@ -59,9 +59,9 @@ export default class Footer extends Component {
                 ><i class="fa fa-map-marker" style="font-size: 30px"></i></a
             ></li>
           </ul>
-        </div>
+        </div> */}
 
-        <div class="map">
+        {/* <div class="map">
           <h6>1111, Seymor Street, Vancouver, BC</h6>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10410.510865752853!2d-123.1152548!3d49.2834511!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb4e03bf432e66714!2sBCIT%20Downtown%20Campus!5e0!3m2!1sen!2sca!4v1602882765225!5m2!1sen!2sca"
@@ -73,19 +73,19 @@ export default class Footer extends Component {
             aria-hidden="false"
             tabindex="0"
           ></iframe>
-        </div>
+        </div> */}
       </div>
       {/* <!-- End of Container Footer --> */}
 
       {/* <!-- Copyright--> */}
-      <div class="copyright">
+      {/* <div class="copyright">
         <p class="m-0 text-center text-white"
           >Copyright &copy; <a href="index.html">WalkaBooks 2020</a> <br />
           <a href="#">Terms & Conditions</a> | <a href="#">Privacy Policy </a> |
           <a href="#"> Site Map </a> |
           <a href="credits.html">Photo Credits</a></p
         >
-      </div>
+      </div> */}
       {/* <!-- End of Copyright --> */}
     </footer>
     {/* <!-- End of Footer --> */}
