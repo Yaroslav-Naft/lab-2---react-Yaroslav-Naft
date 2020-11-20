ccd5559ba3df3d415ee59f7a0c942f395a0a7d6f
Yaroslav-Naft <ynaftulyev@my.bcit.ca>
2020-11-19 18:47:43 -0800

    commit

--- my-app/src/Movies.js
+++ my-app/src/Movies.js
@@ -1,40 +1,40 @@
 import React, { Component } from 'react'
 import movieData from "./data/movieData";
-import MoviesItem from "./MoviesItem";
+import movieDataItem from "./movieDataItem";
 
 function makeMovie(movieData){
-    return  <MoviesItem 
+    return  <movieDataItem 
     key = {movieData.id}
     poster = {movieData.moviePoster}
     title = {movieData.moviePoster}
     date = {movieData.moviePoster} />;
 }
 
-export default class Movies extends Component {
+export default class movieData extends Component {
 
 constructor(props){
     super(props);
     this.state = {
-        movies: [],
+        movieData: [],
         rendered: false
     }
 }
 //When component has been sucessfully rendered react will automoatically call component did mount
 componentDidMount(){
     fetch('https://api.themoviedb.org/3/movie/550?api_key=4bb3e5c49b10d81303e2fbea269898af')
-    //we convert moviesresult from API to JSON
-    .then(moviesresult => moviesresult.json())
+    //we convert movieDataresult from API to JSON
+    .then(movieDataresult => movieDataresult.json())
     //We pass the JSON data to the state of our component
     .then(json => {
         this.setState({
-            movies: [],
+            movieData: [],
             rendered: true
          })
     })
 }
     render() {
 
-        const {movies, rendered} = this.state;
+        const {movieData, rendered} = this.state;
         return (
                 <div class="container">
                     <div className="row my-2 mx-2">
